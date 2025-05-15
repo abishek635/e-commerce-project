04a14d5707310c139f636316a6049fd64ae68a1f
abishek <abishekchiyaa@gmail.com>
2025-05-15 15:31:20 +0530

    generate the html for the homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import './HomePage.css';
 import './header.css'
+import { products } from '../../html-css-template/data/products';
 export function HomePage() {
     return (
         <>
@@ -37,54 +38,61 @@ export function HomePage() {
 
             <div className="home-page">
                 <div className="products-grid">
-                    <div className="product-container">
-                        <div className="product-image-container">
-                            <img className="product-image"
-                                src="assets/products/athletic-cotton-socks-6-pairs.jpg" />
-                        </div>
+                    {products.map((product) => {
+                        return (
+                            < >
+                                <div key = {product.id} className="product-container">
+                                    <div className="product-image-container">
+                                        <img className="product-image"
+                                            src={product.image}/>
+                                    </div>
 
-                        <div className="product-name limit-text-to-2-lines">
-                            Black and Gray Athletic Cotton Socks - 6 Pairs
-                        </div>
+                                    <div className="product-name limit-text-to-2-lines">
+                                        {product.name}
+                                    </div>
 
-                        <div className="product-rating-container">
-                            <img className="product-rating-stars"
-                                src="assets/ratings/rating-45.png" />
-                            <div className="product-rating-count link-primary">
-                                87
-                            </div>
-                        </div>
+                                    <div className="product-rating-container">
+                                        <img className="product-rating-stars"
+                                            src={`assets/ratings/rating-${product.rating.stars *10}`} />
+                                        <div className="product-rating-count link-primary">
+                                            {product.rating.count}
+                                        </div>
+                                    </div>
 
-                        <div className="product-price">
-                            $10.90
-                        </div>
+                                    <div className="product-price">
+                                       ${(product.priceCents / 100).toFixed(2)}
+                                    </div>
 
-                        <div className="product-quantity-container">
-                            <select>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                            </select>
-                        </div>
+                                    <div className="product-quantity-container">
+                                        <select>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                            <option value="6">6</option>
+                                            <option value="7">7</option>
+                                            <option value="8">8</option>
+                                            <option value="9">9</option>
+                                            <option value="10">10</option>
+                                        </select>
+                                    </div>
 
-                        <div className="product-spacer"></div>
+                                    <div className="product-spacer"></div>
 
-                        <div className="added-to-cart">
-                            <img src="assets/icons/checkmark.png" />
-                            Added
-                        </div>
+                                    <div className="added-to-cart">
+                                        <img src="assets/icons/checkmark.png" />
+                                        Added
+                                    </div>
 
-                        <button className="add-to-cart-button button-primary">
-                            Add to Cart
-                        </button>
-                    </div>
+                                    <button className="add-to-cart-button button-primary">
+                                        Add to Cart
+                                    </button>
+                                </div>
+                            </>
+                        );
+                    })}
+
 
 
 
